e11da641bcd01970ee22c71c2154f1bbd4ac318f
PhamGiaLuong <pham15032004@gmail.com>
2024-09-30 14:36:55 +0700

    init Ex6

--- STM32/Core/Src/SystemTimer.c
+++ STM32/Core/Src/SystemTimer.c
@@ -6,34 +6,149 @@
  */
 #include "SystemTimer.h"
 
-int timer1Flag = 0;
-int timer1Counter = 0;
+#define MAX_TIMERS 10
 
-int timer2Flag = 0;
-int timer2Counter = 0;
+int flag[MAX_TIMERS] = {0};
+int timerCounter[MAX_TIMERS] = {0};
+int counter = 1000;
 
-void setTimer1(int duration){
-	timer1Counter = duration;
-	timer1Flag = 0;
+//*************** Start implementation of Priority Queue ***************
+typedef struct{
+	int timer;
+	int priority;
+} event;
+
+typedef struct{
+	event data[MAX_TIMERS];
+	int size;
+} priorityQueue;
+
+priorityQueue pq;
+
+void initQueue(priorityQueue *pq){
+	pq->size = 0;
 }
 
-void setTimer2(int duration){
-	timer2Counter = duration;
-	timer2Flag = 0;
+void swapElements(event *a, event *b){
+	event temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
+// Max-heap
+void heapifyUp(priorityQueue *pq, int index){
+	int parentIndex = (index - 1) / 2;
+	if (index > 0 && pq->data[index].priority > pq->data[parentIndex].priority) {
+		swapElements(&pq->data[index], &pq->data[parentIndex]);
+		heapifyUp(pq, parentIndex);
+	}
+}
+
+void heapifyDown(priorityQueue *pq, int index) {
+	int leftChild = 2 * index + 1;
+	int rightChild = 2 * index + 2;
+	int largest = index;
+
+	if (leftChild < pq->size && pq->data[leftChild].priority > pq->data[largest].priority) {
+		largest = leftChild;
+	}
+	if (rightChild < pq->size && pq->data[rightChild].priority > pq->data[largest].priority) {
+		largest = rightChild;
+	}
+
+	if (largest != index) {
+		swapElements(&pq->data[index], &pq->data[largest]);
+		heapifyDown(pq, largest);
+	}
+}
+
+void enqueue(priorityQueue *pq, int timer, int priority) {
+	if (pq->size < MAX_TIMERS) {
+		pq->data[pq->size].timer = timer;
+		pq->data[pq->size].priority = priority;
+		heapifyUp(pq, pq->size);
+		pq->size++;
+	}
+}
+
+void dequeue(priorityQueue *pq) {
+	pq->data[0] = pq->data[pq->size - 1];
+	pq->size--;
+	heapifyDown(pq, 0);
+}
+
+void addTimer(priorityQueue *pq, int timer, int priority) {
+	static priorityQueue tmpQueue;
+	static int tmpInit = 0;
+
+	if (!tmpInit) {
+		initQueue(&tmpQueue);
+		tmpInit = 1;
+	}
+
+	if (priority > counter) {
+		enqueue(&tmpQueue, timer, priority);
+	} else {
+		enqueue(pq, timer, priority);
+	}
+
+	if (pq->size == 0 && tmpQueue.size > 0) {
+		*pq = tmpQueue;
+		initQueue(&tmpQueue);
+		tmpInit = 0;
+	}
+}
+//*************** End implementation of Priority Queue ***************
+
+void initTimer(){
+    initQueue(&pq);
+}
+int isTimerFlagOn(int index){
+	return (flag[index-1] == 1) ? 1 : 0;
+}
+//void setTimer(int index, int duration){
+//    if (index-1 >= 0 && index-1 < MAX_TIMERS) {
+//        flag[index-1] = 0;
+//        int priority = counter - duration;
+//        if (priority < 0) priority += 1000;
+//        addTimer(&pq, index-1, priority);
+//    }
+//}
+//void timerRun(){
+//	if (pq.data[0].priority == counter){
+//		flag[pq.data[0].timer] = 1;
+//		dequeue(&pq);
+//	}
+//	counter--;
+//	if (counter < 0) counter = 1000;
+//}
+void setTimer(int timer, int duration){
+	flag[timer-1] = 0;
+	timerCounter[timer-1] = duration;
+}
 void timerRun(){
-	if (timer1Counter > 0){
-		timer1Counter--;
-		if (timer1Counter <= 0){
-			timer1Flag = 1;
+	if (timerCounter[0] > 0){
+		timerCounter[0]--;
+		if (timerCounter[0] <= 0){
+			flag[0] = 1;
 		}
 	}
-	if (timer2Counter > 0){
-			timer2Counter--;
-			if (timer2Counter <= 0){
-				timer2Flag = 1;
-			}
+	if (timerCounter[1] > 0){
+		timerCounter[1]--;
+		if (timerCounter[1] <= 0){
+			flag[1] = 1;
 		}
+	}
+	if (timerCounter[2] > 0){
+		timerCounter[2]--;
+		if (timerCounter[2] <= 0){
+			flag[2] = 1;
+		}
+	}
+	if (timerCounter[3] > 0){
+		timerCounter[3]--;
+		if (timerCounter[3] <= 0){
+			flag[3] = 1;
+		}
+	}
 }
-
--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -172,16 +172,17 @@ HAL_TIM_Base_Start_IT(&htim2);
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-setTimer1(50);
-setTimer2(65);
+setTimer(1, 50);
+setTimer(2, 65);
+setTimer(3, 1000);
   while (1)
   {
-	  if (timer1Flag == 1){
-		  setTimer1(100);
+	  if (isTimerFlagOn(1) == 1){
+		  setTimer(1, 100);
 		  HAL_GPIO_TogglePin(GPIOA, DOT_Pin);
 	  }
-	  if (timer2Flag == 1){
-		  setTimer2(25);
+	  if (isTimerFlagOn(2) == 1){
+		  setTimer(2, 25);
 		  second++;
 		  if (second >= 60){
 			  second = 0;
@@ -196,6 +197,10 @@ setTimer2(65);
 		  }
 		  updateClockBuffer();
 		  update7SEG(index_led++);
+	  }
+	  if (isTimerFlagOn(3) == 1){
+		  setTimer(3, 2000);
+		  HAL_GPIO_TogglePin(GPIOA, LED_RED_Pin);
 	  }
 
 
